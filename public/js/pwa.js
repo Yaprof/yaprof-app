300892c8b00b81ac0372f0a0ac49bbe61ddee8ac
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2023-04-02 17:01:20 +0200

    [FIX] Pwa prompt show promotionnal message

--- public/js/pwa.js
+++ public/js/pwa.js
@@ -6,7 +6,6 @@ window.addEventListener('beforeinstallprompt', (e) => {
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
   // Update UI notify the user they can install the PWA
-  showInstallPromotion();
   // Optionally, send analytics event that PWA install promo was shown.
   console.log(`'beforeinstallprompt' event was fired.`);
 });

caa3a67be9bbddc1e958d5b3c3ffab7818c0cb6d
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2023-04-02 21:37:46 +0200

    [FIX] Middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     try {
-        let isAuth = (window?.localStorage.getItem('token') && window.localStorage.getItem('username') && window.localStorage.getItem('password') && window.localStorage.getItem('ent') && window.localStorage.getItem('url') && window.localStorage.get('user')) ? true : false
+        let isAuth = (window?.localStorage.getItem('token') && window.localStorage.getItem('username') && window.localStorage.getItem('password') && window.localStorage.getItem('ent') && window.localStorage.getItem('url') && window.localStorage.getItem('user')) ? true : false
         console.log(from.path)
         if (isAuth == false && to.path !== '/auth/login') {
             return navigateTo('/auth/login')

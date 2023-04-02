7c31c11859f5c7b8b6a041905ee2b4ed9473b03a
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2023-04-03 00:02:38 +0200

    [FIX] Try to fix sidebar not showing up on mobile

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full fixed top-0 left-0 h-20 flex justify-between items-center p-5 bg-white dark:bg-dark bg-opacity-80 backdrop-blur-lg z-[99]">
+    <nav class="w-full fixed top-0 left-0 h-20 flex justify-between items-center p-5 bg-white dark:bg-dark bg-opacity-80 backdrop-blur-lg z-50">
         <Toast v-for="error in errors" :key="error.message" :data="{message:error.message, color: error.color}" ></Toast>
         <div id="sidebar-toggle" class="flex text-dark dark:text-white gap-5 items-center cursor-pointer">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
@@ -15,7 +15,7 @@
             </div>
             <img :src="userInfos?.profile?.pp" class="w-10 h-10 object-cover object-center rounded-full shadow-md" />
         </div>
-    </div>
+    </nav>
 </template>
 
 <script>

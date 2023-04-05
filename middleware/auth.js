a4e9e1042f4ac3c9d7880a00a92448d26de4f37e
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2023-04-05 19:41:42 +0200

    [ADD] how its work page for ios

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,12 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     try {
+        console.log(window.matchMedia('(display-mode: standalone)').matches)
+        console.log(navigator.platform)
+        if (!window.matchMedia('(display-mode: standalone)').matches && /^iP|MacIntel/.test(navigator.platform)) {
+            return navigateTo('/howitswork')
+        }
+
         let isAuth = (window?.localStorage.getItem('token') && window.localStorage.getItem('username') && window.localStorage.getItem('password') && window.localStorage.getItem('ent') && window.localStorage.getItem('url') && window.localStorage.getItem('user')) ? true : false
-        console.log(from.path)
         if (isAuth == false && to.path !== '/auth/login') {
             return navigateTo('/auth/login')
         }else if (to.path === '/auth/login' && isAuth == true) {
---
+++ pages/howitswork.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <div class="p-5 py-10">
+        <img src="~/public/icons/icon_144x144.png" class="w-20 mb-5"/>
+        <div class="flex flex-col pb-2">
+            <h1 class="text-dark dark:text-white text-3xl font-bold">Yaprof.fr</h1>
+        </div>
+        <p class="text-dark dark:text-white text-2xl pb-5">Comment installer l'application ?</p>
+
+        <p class="text-primary text-xl font-medium pb-3">Étape n°1</p>
+        <img src="~/public/IMG_0748.png" class="w-full mb-5 rounded-xl border border-light dark:border-secondary"/>
+        <p class="text-primary text-xl font-medium pb-3">Étape n°2</p>
+        <img src="~/public/IMG_0749.png" class="w-full mb-5 rounded-xl border border-light dark:border-secondary"/>
+        <p class="text-primary text-xl font-medium pb-3">Étape n°3</p>
+        <img src="~/public/IMG_0750.png" class="w-full mb-5 rounded-xl border border-light dark:border-secondary"/>
+        <p class="text-dark dark:text-white text-2xl font-medium pb-3 w-full text-center pt-5">Et voilà ! 🎉</p>
+    </div>
+</template>
+
+<script>
+
+</script>

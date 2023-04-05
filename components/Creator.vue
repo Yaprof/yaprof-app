a284b1a0d9d119a3071639ac4c26cddffbec4d72
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2023-04-05 09:38:00 +0200

    [ADD] sortie pédagogique reason creator post

--- components/Creator.vue
+++ components/Creator.vue
@@ -32,7 +32,7 @@
                         </div>
                     </div>
                     <div class="w-full h-[1px] bg-neutral-300 dark:bg-neutral-800"></div>
-                    <div class="flex items-center gap-2">
+                    <div class="flex items-center gap-2">   
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 text-dark dark:text-white">
                             <path fill-rule="evenodd" d="M10 2c-1.716 0-3.408.106-5.07.31C3.806 2.45 3 3.414 3 4.517V17.25a.75.75 0 001.075.676L10 15.082l5.925 2.844A.75.75 0 0017 17.25V4.517c0-1.103-.806-2.068-1.93-2.207A41.403 41.403 0 0010 2z" clip-rule="evenodd" />
                         </svg>
@@ -41,6 +41,7 @@
                             <option class="w-full" value="Autre">Autre</option>
                             <option class="w-full" value="Grève">Grève</option>
                             <option class="w-full" value="Maladie">Maladie</option>
+                            <option class="w-full" value="Sortie pédagogique">Sortie pédagogique</option>
                         </select>
 
                     </div>

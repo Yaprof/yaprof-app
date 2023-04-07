1f8463ada7599220a5c39d33597e3d350dedefdf
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2023-04-07 13:24:06 +0200

    [FIX] remove tuto

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,8 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     try {
-        if (!window.matchMedia('(display-mode: standalone)').matches && /^iP|MacIntel/.test(navigator.platform)) {
+        /* if (!window.matchMedia('(display-mode: standalone)').matches && /^iP|MacIntel/.test(navigator.platform)) {
             return navigateTo('/howitswork')
-        }
+        } */
 
         let isAuth = (window?.localStorage.getItem('token') && window.localStorage.getItem('username') && window.localStorage.getItem('password') && window.localStorage.getItem('ent') && window.localStorage.getItem('url') && window.localStorage.getItem('user')) ? true : false
         if (isAuth == false && to.path !== '/auth/login') {

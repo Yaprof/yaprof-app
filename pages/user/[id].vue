7bd6542035d085d6990a999064599ba8c70ee978
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2023-04-12 21:27:16 +0200

    [FIX] try to fix back arrow

--- pages/user/[id].vue
+++ pages/user/[id].vue
@@ -1,8 +1,8 @@
 <template>
     <NuxtLayout>
         <div>
-            <div class="flex items-center gap-3 pb-5 ">
-                <button @click="this.$router.go(-1)" class="active:scale-95 transition-all cursor-pointer">
+            <div class="flex items-center gap-3 pb-3 -mt-2">
+                <button @click="goToPrev()" class="active:scale-95 transition-all cursor-pointer p-2 z-50">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-8 h-8">
                         <path fill-rule="evenodd" d="M17 10a.75.75 0 01-.75.75H5.612l4.158 3.96a.75.75 0 11-1.04 1.08l-5.5-5.25a.75.75 0 010-1.08l5.5-5.25a.75.75 0 111.04 1.08L5.612 9.25H16.25A.75.75 0 0117 10z" clip-rule="evenodd" />
                     </svg>
@@ -11,7 +11,7 @@
             </div>
             <div class="flex flex-col gap-2">
                 <div class="w-full shadow-md relative rounded-xl overflow-hidden">
-                    <img class="w-full object-cover object-center absolute top-0 left-0 h-full blur-lg scale-150 brightness-110 dark:brightness-90" :src="user.profile?.pp" />
+                    <img class="w-full object-cover object-center absolute top-0 left-0 h-full blur-lg scale-150 brightness-110 dark:brightness-90 rounded-xl" :src="user.profile?.pp" />
                     <div class="flex flex-col p-5">
                         <div class="flex items-center gap-1">
                             <img class="mb-2 h-16 w-16 object-cover z-10 rounded-full object-center mr-2" :src="user.profile?.pp" />
@@ -46,6 +46,11 @@ export default {
         return {
             user: {},
             config: {api: this.$config.API_URL, pronote: this.$config.PRONOTE_API_URL}
+        }
+    },
+    methods: {
+        goToPrev() {
+            window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
         }
     },
     async mounted() {

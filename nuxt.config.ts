30e09292807539f918571989c55b77f2086e2f1b
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2023-04-29 23:31:45 +0200

    try to fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -99,7 +99,9 @@ export default defineNuxtConfig({
     },
     pwa: {
         workbox: {
-            swURL: '/sw.js',
+            importScripts: [
+                'sw.js'
+            ],
         },
     },
 })

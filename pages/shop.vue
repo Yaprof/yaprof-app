06aa9bc1d903edcf565484ec8e50ac34063b7f0f
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2023-05-05 22:13:02 +0200

    [FIX] get badges error

--- pages/shop.vue
+++ pages/shop.vue
@@ -18,7 +18,7 @@
                             <img class="w-12 h-12 mt-5" :src="'https://res.cloudinary.com/dzg9awmm8/image/upload/v1682000898/badges/'+badge.id+'.webp'" />
                         </div>
                         <div :class="(user.profile.badges?.find(b => b == badge.id) ? 'bg-light text-dark dark:bg-secondary dark:text-white' : 'bg-primary text-dark') + ' flex items-center justify-center w-full  py-2.5'">
-                            <p class="font-semibold group-active:scale-95 transition-all"> {{ user.profile.badges?.find(b => b == badge.id) ? 'Acheté' : 'Acheter'}} </p>
+                            <p class="font-semibold group-active:scale-95 transition-all"> {{ user?.profile?.badges?.find(b => b == badge.id) ? 'Acheté' : 'Acheter'}} </p>
                         </div>
                         <div class="flex bg-primary bg-opacity-30 rounded-full py-1 pl-4 pr-2 items-center gap-1 absolute top-1 right-1">
                             <p class="text-dark dark:text-white">{{ badge.price || 0 }}</p>
@@ -29,7 +29,7 @@
                 <div :class="(darkOpacity ? 'bg-dark bg-opacity-50' : ' opacity-0') + (pointer_event ? '': ' pointer-events-none') + ' fixed top-0 left-0 h-screen w-full z-[100] transition-all'"></div>
                 <Transition name="slide-down" mode="out-in">
                     <div v-if="popup_buy"  :class="(popup_buy ? '': 'translate-y-full') + ' bg-light dark:bg-secondary p-5 pb-14 fixed bottom-0 left-0 w-full flex flex-col items-center justify-center gap-3 text-dark dark:text-white transition-all z-[101]'" v-click-outside="closePopupBuy">
-                        <div v-if="user.profile.badges?.find(b => b == popup_buy.id)" :class="'bg-slate-200 dark:bg-slate-500 text-dark dark:text-white flex items-center justify-center gap-2 group cursor-pointer py-3 px-6 rounded-xl w-full transition-all ' + (loadingBuy ? 'bg-primaryhover' : '')">
+                        <div v-if="user?.profile?.badges?.find(b => b == popup_buy.id)" :class="'bg-slate-200 dark:bg-slate-500 text-dark dark:text-white flex items-center justify-center gap-2 group cursor-pointer py-3 px-6 rounded-xl w-full transition-all ' + (loadingBuy ? 'bg-primaryhover' : '')">
                             <p>Déjà acheté</p>
                         </div>
                         <div v-else-if="popup_buy.price > user?.profile?.coins" :class="'bg-slate-200 dark:bg-slate-500 text-dark dark:text-white flex items-center justify-center gap-2 group cursor-pointer py-3 px-6 rounded-xl w-full transition-all ' + (loadingBuy ? 'bg-primaryhover' : '')">
@@ -92,7 +92,7 @@ export default {
                 this.errors.push({ message: 'Impossible de récupérer le badge', color: 'danger' })
                 this.closePopupBuy()
             }
-            if (this.user.profile.badges.find(b => b == id)){
+            if (this.user?.profile?.badges.find(b => b == id)){
                 this.errors.push({ message: 'Vous avez déjà ce badge', color: 'danger' })
                 this.closePopupBuy()
             }
@@ -102,7 +102,7 @@ export default {
                 this.loadingBuy = false
                 return this.errors.push({ message: badge.error, color: 'danger' })
             }
-            let badges = this.user.profile.badges
+            let badges = this.user?.profile?.badges
             badges.push(badge.id)
             this.user.profile.badges = badges
             this.errors.push({ message: 'Vous venez d\'acheter le badge', color: 'success' })

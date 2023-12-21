868be77182f98aa299ad3c59e2b21dfae3c91f2b
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2023-12-21 10:36:19 +0100

    Refactor auth middleware to improve code readability and maintainability

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,25 +1,34 @@
-import pkg from '~/package.json'
+import pkg from "~/package.json";
 
 export default defineNuxtRouteMiddleware((to, from) => {
     try {
-        /* if (!window.matchMedia('(display-mode: standalone)').matches) {
+        /*
+        if (!window.matchMedia('(display-mode: standalone)').matches) {
             return window.location.replace('/howitswork')
         } else if (window.location.pathname === '/howitswork') {
             return window.location.replace('/')
         } */
 
-        /* let isAuth = (window?.localStorage.getItem('token') && window?.localStorage.getItem('userInfos') && window?.localStorage.getItem('user')) ? true : false
-        if (isAuth == false && to.path !== '/auth/login') {
-            return window.location.replace('/auth/login')
-        } else if (to.path === '/auth/login' && isAuth == true) {
-            if (window?.localStorage.getItem('version') !== pkg['version']) 
-                window.location.replace('/changelog')
-            return
+        let isAuth =
+            window?.localStorage.getItem("token") &&
+            window?.localStorage.getItem("userInfos") &&
+            window?.localStorage.getItem("user")
+                ? true
+                : false;
+        if (isAuth == false && to.path !== "/auth/login") {
+            return window.location.replace("/auth/login");
+        } else if (to.path === "/auth/login" && isAuth == true) {
+            if (window?.localStorage.getItem("version") !== pkg["version"])
+                window.location.replace("/changelog");
+            return;
         } else {
-            if (window?.localStorage.getItem('version') !== pkg['version'] && isAuth == true) 
-                window.location.replace('/changelog')
-        } */
+            if (
+                window?.localStorage.getItem("version") !== pkg["version"] &&
+                isAuth == true
+            )
+                window.location.replace("/changelog");
+        }
     } catch (e) {
-        return
+        return;
     }
-})
+});
